39c3ac10ea0b6d4151edcf4247648638c49e9783
Romeo75 <58041019+Romeo75@users.noreply.github.com>
2021-04-09 22:58:08 +0200

    Delete TE2_scalar directory

--- TE2_scalar/util.h
+++
@@ -1,4 +0,0 @@
-#include <Eigen/Dense>
-#include <fstream>
-
-void ecrit( std::string name, const Eigen::VectorXf &X, const Eigen::VectorXf &Y_calc, const Eigen::VectorXf &Y_th, const Eigen::VectorXf &Delta );
