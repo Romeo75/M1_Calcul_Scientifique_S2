8bcd520344867e1505d96f7cd1860a6d220ab1c6
Romeo75 <58041019+Romeo75@users.noreply.github.com>
2021-04-09 22:58:22 +0200

    Delete TE2_vector directory

--- TE2_vector/util.h
+++
@@ -1,4 +0,0 @@
-#include <Eigen/Dense>
-#include <fstream>
-
-void ecrit( std::string name, float step, const Eigen::VectorXf &X, const Eigen::MatrixXf &Y_calc, const Eigen::MatrixXf &Y_th, const Eigen::MatrixXf &Delta );
